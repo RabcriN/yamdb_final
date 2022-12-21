6109b0c411ad602c2a22e0e1b57768f9636af847
unknown <user@DESKTOP-VI1L6FA.office.tut.ru>
2022-12-21 21:40:30 +0300

    isorted

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -2,7 +2,6 @@ import os
 from datetime import timedelta
 from distutils.util import strtobool
 
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default="p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs")
